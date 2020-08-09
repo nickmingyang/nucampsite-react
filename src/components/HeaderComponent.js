812bb92a8a1e50f8d4f42830e3e627731745a5ff
nickming <nickmingyang@gmail.com>
2020-08-09 17:26:50 -0400

    Adding to the Single Page App

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -18,10 +18,10 @@ class Header extends Component {
       isNavOpen: false,
     };
   }
-  toggleNav(){
-      this.setState({
-          isNavOpen: !this.state.isNavOpen
-      })
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen,
+    });
   }
   render() {
     return (
@@ -38,31 +38,38 @@ class Header extends Component {
         </Jumbotron>
         <Navbar dark sticky="top" expand="md">
           <div className="container">
-            <NavbarBrand className="mr-auto" href="/"><img src = "/assets/images/logo.png" height="30" width="30" alt="NuCamp Logo" /></NavbarBrand>
-            <NavbarToggler onClick = {this.toggleNav} />
-            <Collapse isOpen = {this.state.isNavOpen} navbar>
-                <Nav navbar>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/home">
-                        <i className="fa fa-home fa-lg" /> Home
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/directory">
-                        <i className="fa fa-list fa-lg" /> Directory
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/aboutus">
-                        <i className="fa fa-info fa-lg" /> About
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/contactus">
-                        <i className="fa fa-address-card fa-lg" /> Contact Us
-                        </NavLink>
-                    </NavItem>
-                </Nav>
+            <NavbarBrand className="mr-auto" href="/">
+              <img
+                src="/assets/images/logo.png"
+                height="30"
+                width="30"
+                alt="NuCamp Logo"
+              />
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggleNav} />
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink className="nav-link" to="/home">
+                    <i className="fa fa-home fa-lg" /> Home
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/directory">
+                    <i className="fa fa-list fa-lg" /> Directory
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/aboutus">
+                    <i className="fa fa-info fa-lg" /> About
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/contactus">
+                    <i className="fa fa-address-card fa-lg" /> Contact Us
+                  </NavLink>
+                </NavItem>
+              </Nav>
             </Collapse>
           </div>
         </Navbar>
